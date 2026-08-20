cf46128eb379f095f8a0e77a6aba3fca827d7626
GuilhermeCavalcanti-hub <272444@unifio.edu.br>
2026-08-20 01:13:59 +0000

    Criação do projeto

---
+++ ecommerce/src/main/java/br/edu/unifio/ecommerce/entidades/Categoria.java
@@ -0,0 +1,14 @@
+package br.edu.unifio.ecommerce.entidades;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class Categoria {
+    private Short id;
+    private String nome;
+    private String descricao;
+}
