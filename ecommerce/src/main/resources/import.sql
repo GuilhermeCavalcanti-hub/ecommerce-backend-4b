440753033fa1f8c84306ed4d261e08233f1ac4fa
GuilhermeCavalcanti-hub <272444@unifio.edu.br>
2026-08-27 01:36:55 +0000

    Parte 4

---
+++ ecommerce/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into categoria (nome, descricao) values ('Informática', 'Produtos de Informática' );
+
+insert into categoria (nome, descricao) values ('Livros', 'Livros Tecnicos');
+
+insert into produto (nome, descricao, preco, estoque, categoria_id) values ('Código Limpo', 'Livro Do Autor Robert C. Martin', 87.34, 20, 2);
